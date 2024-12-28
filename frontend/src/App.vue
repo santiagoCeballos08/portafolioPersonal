ff500fe78330cf1295db97d01b637278bab41661
santiago ceballos <santiceballos44@outlook.com>
2024-12-28 12:45:17 -0500

    refactor(frontend): :construction: creacion frontend con vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>
+		<h1>portafolio</h1>
+	</div>
+</template>
+
+<style scoped></style>
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	purge: false,
-	content: ['./public/**/*.html', './assets/**/*.{js,jsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
