2e9ac0b625922e0fd3c62178fb74d88471df09a8
santiago ceballos <santiceballos44@outlook.com>
2024-12-30 23:16:03 -0500

    feat(services): :sparkles: end services section

--- frontend/src/components/ServiceComponent.vue
+++ frontend/src/components/ServiceComponent.vue
@@ -12,30 +12,177 @@
 
 			<div class="flex w-full items-center justify-center">
 				<div class="grid w-full gap-10 p-2 grid-cols-1 md:grid-cols-3 md:grid-rows-5 min-h-[50rem]">
-					<div
-						class="md:col-span-1 md:row-span-4 bg-gray-200 rounded-lg shadow-md flex items-center justify-center">
-						1
-					</div>
+					<article
+						class="bg-secundaryColor md:col-span-1 md:row-span-4 rounded-lg shadow-md flex flex-col items-center p-4">
+						<div class="card__header__info">
+							<h2 class="text-5xl font-bold capitalize">servicios</h2>
+						</div>
+						<div class="conent__service__box grid grid-cols-2 grid-rows-3 gap-4 pt-20">
+							<div
+								class="box service__box bg-primaryColor p-4 rounded-md flex flex-col items-center text-center">
+								<font-awesome-icon icon="code" class="h-[4rem]" />
+								desarrollo web
+							</div>
+							<div
+								class="box service__box bg-primaryColor p-4 rounded-md flex flex-col items-center text-center">
+								<font-awesome-icon icon="person-chalkboard" class="h-[4rem]" />
+								consultoria
+							</div>
+							<div
+								class="box service__box bg-primaryColor p-4 rounded-md flex flex-col items-center text-center">
+								<font-awesome-icon icon="fa-brands fa-figma" class="h-[4rem]" />
 
-					<div
-						class="md:col-span-1 md:row-span-3 bg-gray-200 rounded-lg shadow-md flex items-center justify-center">
-						2
-					</div>
+								diseño web
+							</div>
+							<div
+								class="box service__box bg-primaryColor p-4 rounded-md flex flex-col items-center text-center">
+								<font-awesome-icon icon="mobile" class="h-[4rem]" />
 
-					<div
-						class="md:col-span-1 md:row-span-2 bg-gray-200 rounded-lg shadow-md flex items-center justify-center">
-						3
-					</div>
+								desarrollo movil
+							</div>
+							<div
+								class="box service__box bg-primaryColor p-4 rounded-md flex flex-col items-center text-center">
+								<font-awesome-icon icon="chart-line" class="h-[4rem]" />
+								análisis de datos
+							</div>
+						</div>
+					</article>
 
-					<div
-						class="md:col-span-1 md:row-span-2 bg-gray-200 rounded-lg shadow-md flex items-center justify-center">
-						4
-					</div>
+					<article
+						class="bg-secundaryColor md:col-span-1 md:row-span-3 rounded-lg shadow-md flex flex-col items-center justify-center p-4">
+						<div class="card__header__info">
+							<h2 class="text-5xl font-bold capitalize mb-14">Herramientas</h2>
+						</div>
+						<div class="content__service__tools grid grid-cols-5 gap-4">
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://angular.dev/"
+								><img height="50" src="https://skillicons.dev/icons?i=angular"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://vuejs.org/"
+								><img height="50" src="https://skillicons.dev/icons?i=vue"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://nodejs.org/en"
+								><img height="50" src="https://skillicons.dev/icons?i=nodejs"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://www.php.net/"
+								><img height="50" src="https://skillicons.dev/icons?i=php"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://developer.mozilla.org/es/docs/Web/HTML/Element/html"
+								><img height="50" src="https://skillicons.dev/icons?i=html"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://developer.mozilla.org/es/docs/Web/CSS"
+								><img height="50" src="https://skillicons.dev/icons?i=css"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://developer.mozilla.org/es/docs/Web/JavaScript"
+								><img height="50" src="https://skillicons.dev/icons?i=js"
+							/></a>
 
-					<div
-						class="md:col-span-1 md:row-span-3 bg-gray-200 rounded-lg shadow-md flex items-center justify-center">
-						5
-					</div>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://www.typescriptlang.org/"
+								><img height="50" src="https://skillicons.dev/icons?i=ts"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://tailwindcss.com/"
+								><img height="50" src="https://skillicons.dev/icons?i=tailwind"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://getbootstrap.com/"
+								><img height="50" src="https://skillicons.dev/icons?i=bootstrap"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://www.mysql.com/"
+								><img height="50" src="https://skillicons.dev/icons?i=mysql"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://laravel.com/"
+								><img height="50" src="https://skillicons.dev/icons?i=laravel"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://flutter.dev/"
+								><img height="50" src="https://skillicons.dev/icons?i=flutter"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://www.figma.com/"
+								><img height="50" src="https://skillicons.dev/icons?i=figma"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://www.docker.com/"
+								><img height="50" src="https://skillicons.dev/icons?i=docker"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://code.visualstudio.com/"
+								><img height="50" src="https://skillicons.dev/icons?i=vscode"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://git-scm.com/"
+								><img height="50" src="https://skillicons.dev/icons?i=git"
+							/></a>
+							<a
+								target="_blank"
+								class="hover:translate-y-[-0.5rem] transition-all"
+								href="https://aws.amazon.com/es/"
+								><img height="50" src="https://skillicons.dev/icons?i=aws"
+							/></a>
+						</div>
+					</article>
+
+					<article
+						class="bg-secundaryColor md:col-span-1 md:row-span-2 rounded-lg shadow-md flex justify-center items-center p-4 h-[20rem] service-1">
+						<h3 class="text-5xl uppercase font-bold">Desarrollo web</h3>
+					</article>
+
+					<article
+						class="bg-secundaryColor md:col-span-1 md:row-span-2 rounded-lg shadow-md flex justify-center p-4">
+						<div class="card__header__info">
+							<h2 class="text-5xl font-bold capitalize">habilidades blandas</h2>
+						</div>
+						<div class=""></div>
+					</article>
+
+					<article
+						class="bg-secundaryColor md:col-span-1 md:row-span-3 rounded-lg shadow-md flex justify-center items-center h-[25rem] service-2">
+						<h3 class="text-5xl uppercase font-bold">diseño UI y UX</h3>
+					</article>
 				</div>
 			</div>
 		</div>
@@ -49,5 +196,21 @@
 		background: linear-gradient(0deg, hsla(238, 38%, 19%, 0.747) 10%, hsla(238, 38%, 19%, 0.767) 100%),
 			url(../assets/img/fondo_tuercas.svg);
 		background-size: cover;
+	}
+
+	.service-1 {
+		background: linear-gradient(0deg, hsla(238, 38%, 19%, 0.747), hsla(238, 38%, 19%, 0.747)),
+			url(../assets/img/services_bg-1.webp);
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center;
+	}
+
+	.service-2 {
+		background: linear-gradient(0deg, hsla(238, 38%, 19%, 0.747), hsla(238, 38%, 19%, 0.747)),
+			url(../assets/img/services_bg-2.webp);
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center;
 	}
 </style>
