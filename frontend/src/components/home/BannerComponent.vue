85bc5532023fcba506ad85b44ae2d9bcd2b6d221
santiago ceballos <sceballos180@gmail.com>
2026-09-04 13:32:11 -0500

    feat: configuracion body y fonts

--- frontend/src/components/home/BannerComponent.vue
+++ frontend/src/components/home/BannerComponent.vue
@@ -4,9 +4,14 @@
 
 <template>
 	<article class="main__banner__hero ">
-		<section class="grid col-span-1 main__banner">
-			<p>hola, soy santiago</p>
-			<h1>Desarrollador Fullstak</h1>
+		<section class="grid grid-col-1 main__banner md:grid-cols-2">
+			<picture class="conent__image">
+				<img src="" alt="picture aboutme">
+			</picture>
+			<div>
+				<p>hola, soy santiago</p>
+				<h1>Desarrollador Fullstak</h1>
+			</div>
 		</section>
 	</article>
 </template>
